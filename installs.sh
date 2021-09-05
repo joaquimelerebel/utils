c85aa7e8736d238b6054b64fa5e7020a6f402947
ulysse <ulysse@mac.lan>
2021-09-05 18:58:48 +0200

    zsh themes and terminator theme

--- installs.sh
+++ installs.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
+APT_OPT="-q -y"
+APT_INSTALL="sudo apt-get $APT_OPT install"
+
 set +x
 
 #MacBook Pro keyboard
@@ -7,40 +10,64 @@ cat keyboard >> /etc/default/keyboard
 
 #wifi
 mkdir /lib/firmware/brcm/
-cd /lib/firmware/brcm/
 wget https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/plain/brcm/brcmfmac43602-pcie.bin /lib/firmware/brcm/
-cd
+
 
 #zsh
-sudo apt-get install zsh
+$APT_INSTALL zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-cp .zshrc /home/ulysse/
-sudo cp .zshrc /root
+# cp .zshrc /home/$USER/
+# sudo cp .zshrc /root
 
 #change default shell
 chsh -s $(which zsh)
-sudo -u ulysse chsh -s $(which zsh)
+sudo -u $USER chsh -s $(which zsh)
 
-#theme
-cd /usr/share/themes/
-git clone https://github.com/freefreeno/Materia-Manjaro-Dark.git
-cd
 
-#icons 
+# get the right oh my zsh themes
+sed -i s/robbyrussell/agnoster/g /home/$USER/.zshrc
+sed -i s/robbyrussell/aussiegeek/g /root/.zshrc
+
+sed s+/root/.oh-my-zsh+/home/$USER/.oh-my-zsh+g /home/$USER/.zshrc
 
 
 #terminator
-sudo apt-get install terminator
+$APT_INSTALL terminator
+mkdir -p $HOME/.config/terminator/plugins
+wget https://git.io/v5Zww -O "/home/$USER/.config/terminator/plugins/terminator-themes.py"
+cat terminator-config > "/home/$USER/.config/terminator/config"
+
+
 
 #sublime text
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install apt-transport-https
+$APT_INSTALL apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
-sudo apt-get install sublime-text
+$APT_INSTALL sublime-text
 
+#programming
+$APT_INSTALL vim gcc gdb make python python3 python3-pip 
+$APT_INSTALL doxygen
+
+#virtual machine
+$APT_INSTALL qemu qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils libguestfs-tools genisoimage virtinst libosinfo-bin virt-manager
+sudo adduser $USER libvirt
+sudo adduser $USER libvirt-qemu
+
+#docker
+$APT_INSTALL docker 
+
+#hacking
+#	general
+$APT_INSTALL nmap wireshark jhon aircrack-ng 
+
+#	bins
+$APT_INSTALL binwalk radare2
+#	web
+$APT_INSTALL dirb
 
 #eclipse 
-cd /home/ulysse/Downloads
+cd /home/$USER/Downloads
 wget https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2020-09/R/eclipse-inst-jre-linux64.tar.gz&mirror_id=1099
 
